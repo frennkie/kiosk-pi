5193993b2410e8a472958de0cc9636dad4cde1cd
frennkie <mail@rhab.de>
2016-11-27 16:33:15 +0000

    add reduced bootsrap script (no vim/git)

---
+++ 01s_install-os-basics.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set +x  # debug off
+set -e  # exit on error
+
+# --------------------------------------------------------------------
+echo ""
+echo "APT update + upgrade"
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+
+# --------------------------------------------------------------------
+echo ""
+echo "APT clean up"
+sudo apt-get -y autoclean
+sudo apt-get -y autoremove
+
+# --------------------------------------------------------------------
+echo ""
+echo "Download the scripts from Github manually:"
+echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/02_install-x11-basics.sh"
+echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/03_add-user.sh"
+echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/04_setup-x11.sh"
+echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/kiosk.sh"
+echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/screen_off.sh"
+echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/screen_on.sh"
