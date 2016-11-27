7a081f659d061cae3cc519dd49a6856f53f107c5
frennkie <mail@rhab.de>
2016-11-27 13:42:48 +0000

    split setup and install

--- install-x11-basics.sh
+++ install-x11-basics.sh
@@ -3,7 +3,7 @@ set +x
 set -e
 
 echo ""
-echo "Installing basic X11 packages on HypriotOS"
+echo "Installing basic X11 packages"
 
 # --------------------------------------------------------------------
 echo ""
@@ -25,31 +25,7 @@ echo "...done"
 echo ""
 echo " STEP 2: install X11 and LightDM"
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends xserver-xorg xinit xserver-xorg-video-fbturbo lxde lxde-common lightdm x11-xserver-utils
+sudo apt-get install -y --no-install-recommends xserver-xorg xinit xserver-xorg-video-fbturbo lxde lxde-common lightdm x11-xserver-utils unclutter
 sudo apt-get install -y policykit-1 hal
 echo "...done"
 
-
-# --------------------------------------------------------------------
-echo ""
-echo " STEP 3: enable LightDM autologin for user=pi"
-LIGHTDM_CONF_FILE=/etc/lightdm/lightdm.conf
-if [ ! -f ${LIGHTDM_CONF_FILE}.sav ]; then
-  # backup original file
-  sudo mv ${LIGHTDM_CONF_FILE} ${LIGHTDM_CONF_FILE}.sav
-
-cat << EOF | sudo tee ${LIGHTDM_CONF_FILE} >/dev/null
-[SeatDefaults]
-autologin-user=pi
-autologin-user-timeout=0
-# don't sleep the screen
-xserver-command=X -s 0 dpms
-EOF
-
-fi
-cat ${LIGHTDM_CONF_FILE}
-echo "...done"
-
-# --------------------------------------------------------------------
-echo ""
-echo "...X11 installation done, please reboot"
