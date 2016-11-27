e8cbc7d3c5a02f870152f81bd7b3a325c6dfbac0
Robert Habermann <mail@rhab.de>
2016-11-27 13:09:43 +0100

    remove not needed stuff

--- fbturbo/fbturbo-create-tar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-sudo tar cvfz fbturbo.tar.gz \
-  /usr/lib/xorg/modules/drivers/fbturbo_drv.so \
-  /usr/lib/xorg/modules/drivers/fbturbo_drv.la \
-  /usr/share/man/man4/fbturbo.4 \
-  /usr/share/X11/xorg.conf.d/99-fbturbo.conf
