1f18238b95aeecb5abdad142dd0335d98e0ec4df
Robert Habermann <mail@rhab.de>
2016-11-27 13:03:21 +0100

    remove self compiled fbturbo

--- install-x11-basics.sh
+++ install-x11-basics.sh
@@ -25,14 +25,14 @@ echo "...done"
 echo ""
 echo " STEP 2: install X11 and LightDM"
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends xserver-xorg xinit xserver-xorg-video-fbdev lxde lxde-common lightdm x11-xserver-utils
+sudo apt-get install -y --no-install-recommends xserver-xorg xinit xserver-xorg-video-fbturbo lxde lxde-common lightdm x11-xserver-utils
 sudo apt-get install -y policykit-1 hal
 echo "...done"
 
 
 # --------------------------------------------------------------------
 echo ""
-echo " STEP 3: enable LightDM autologin for user=pirate"
+echo " STEP 3: enable LightDM autologin for user=pi"
 LIGHTDM_CONF_FILE=/etc/lightdm/lightdm.conf
 if [ ! -f ${LIGHTDM_CONF_FILE}.sav ]; then
   # backup original file
@@ -40,25 +40,13 @@ if [ ! -f ${LIGHTDM_CONF_FILE}.sav ]; then
 
 cat << EOF | sudo tee ${LIGHTDM_CONF_FILE} >/dev/null
 [SeatDefaults]
-autologin-user=pirate
+autologin-user=pi
 autologin-user-timeout=0
 EOF
 
 fi
 cat ${LIGHTDM_CONF_FILE}
 echo "...done"
-
-
-# --------------------------------------------------------------------
-echo ""
-echo " STEP 4: install pre-compiled fbturbo"
-FBTURBO_TAR_URL=https://github.com/hypriot/x11-on-HypriotOS/raw/master/fbturbo/fbturbo.tar.gz
-FBTURBO_TAR_TMPFILE=/tmp/fbturbo.tar.gz
-wget -O ${FBTURBO_TAR_TMPFILE} ${FBTURBO_TAR_URL}
-sudo tar xvf ${FBTURBO_TAR_TMPFILE} -C /
-rm -f ${FBTURBO_TAR_TMPFILE}
-echo "...done"
-
 
 # --------------------------------------------------------------------
 echo ""
