7cadfd08d1e3bf78c7d3ea561eebf4a5122547ce
frennkie <mail@rhab.de>
2016-11-27 16:22:17 +0000

    remove mouse pointer

--- kiosk.sh
+++ kiosk.sh
@@ -3,5 +3,8 @@
 
 #export DISPLAY=:0.0
 
+# disable mouse pointer
+unclutter &
+
 # start chromium
 chromium-browser --noerrdialogs --disable-session-crashed-bubble --disable-infobars --kiosk http://co2.rhab.de
