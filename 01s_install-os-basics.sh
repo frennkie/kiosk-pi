331c6328fed7d18a269435d7f007e722cd772282
frennkie <mail@rhab.de>
2016-11-27 18:22:15 +0000

    update

--- 01s_install-os-basics.sh
+++ 01s_install-os-basics.sh
@@ -17,11 +17,13 @@ sudo apt-get -y autoremove
 
 # --------------------------------------------------------------------
 echo ""
-echo "Download the scripts from Github manually:"
-echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/02_install-x11-basics.sh"
-echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/03_install-browser.sh"
-echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/04_add-user.sh"
-echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/05_setup-x11.sh"
-echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/kiosk.sh"
-echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/screen_off.sh"
-echo "wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/screen_on.sh"
+echo "Download the scripts from Github individually:"
+mkdir ~/kiosk-pi
+cd ~/kiosk-pi
+wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/02_install-x11-basics.sh
+wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/03_install-browser.sh
+wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/03_install-browser.sh
+wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/05_setup-x11.sh
+wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/kiosk.sh
+wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/screen_off.sh
+wget https://raw.githubusercontent.com/frennkie/kiosk-pi/master/screen_on.sh
