2b05efb5190180896145334b35c8cf0a56582389
frennkie <mail@rhab.de>
2019-01-23 16:28:05 +0100

    Update screen_on.sh

--- screen_on.sh
+++ screen_on.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 # screen_on.sh
 
-set +x  # debug off 
-#set -x  # debug on
 set -e  # stop on errors
 
 export DISPLAY=:0.0
 
-xset +dpms
-xset dpms force on
-xset -dpms
-
+vcgencmd  display_power 1
